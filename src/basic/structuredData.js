04c88a7ffab0000d05192f8035556c192a8cdf99
sasaken555 <accelerk@gmail.com>
2019-07-20 03:09:34 +0900

    receive JSON data

---
+++ src/basic/structuredData.js
@@ -0,0 +1,14 @@
+const { getLogger } = require("../lib/logUtil");
+const logger = getLogger("structured-data");
+
+const NATS = require("nats");
+const nc = NATS.connect({
+    url: "nats://localhost:4222",
+    json: true
+});
+
+nc.subscribe("updates", msg => {
+    logger.debug(`got message: ${JSON.stringify(msg)}`);
+});
+
+nc.publish("updates", { name: "Hoge", age: 88 });
