0206e67f2f89eb73c19ef9aa6ca4800ebccaaa34
sasaken555 <accelerk@gmail.com>
2019-07-24 00:30:55 +0900

    request-reply style

--- src/basic/requestReply.js
+++ src/basic/requestReply.js
@@ -1,1 +1,23 @@
 "use strict";
+const { getLogger } = require("../lib/logUtil");
+const logger = getLogger("req-res");
+const NATS = require("nats");
+const nc = NATS.connect("nats://localhost:4222");
+
+// setup subscription
+nc.subscribe("time", (msg, reply) => {
+  if (reply) {
+    logger.debug(`subject=time, msg=${msg}, reply=${reply}`);
+    nc.publish(reply, new Date().toLocaleDateString());
+  }
+});
+
+// reply message subscription
+const inbox = NATS.createInbox();
+nc.subscribe(inbox, msg => {
+  logger.debug(`subject=${inbox}, msg=${msg}`);
+  nc.close();
+});
+
+// publish to "time" subject, expect to reply to inbox
+nc.publish("time", "What time is it?", inbox);
